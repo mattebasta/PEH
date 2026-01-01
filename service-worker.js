385a32237bfb0bc300f11f8e48278923f16f7663
mattebasta <77062910+mattebasta@users.noreply.github.com>
2026-01-01 12:13:48 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+self,addEventListener("install",(event)=>{
+    event.waitUntil(
+        caches.open('pwa-cache').then((caches)=>{
+            return caches.addAll([
+                '/',
+                '/index.html'
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", (event)=>{
+    event.respondWith(
+        caches.match(event.request).then((response)=>{
+            return response || fetch(event.request);
+        })
+    );
+});
